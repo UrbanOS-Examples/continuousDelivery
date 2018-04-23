d299a08bb076a572ab57e97274c45c04e5f740cb
Jeffrey Lutz <jeff.lutz@agileanswers.com>
2018-04-23 14:27:51 -0400

    Added ECS service and task definition details along with variable driven settings

--- aws/nexus-ecs/main.tf
+++ aws/nexus-ecs/main.tf
@@ -3,8 +3,9 @@ provider "aws" {
 }
 
 module "ecs-scalable-cluster" "nexus-cluster" {
-  source = "../../../terraform-aws-ecs-scalable-cluster"
+#  source = "../../../terraform-aws-ecs-scalable-cluster"
 #  source  = "anugnes/ecs-scalable-cluster/aws"
+  source = "SmartColumbusOS/ecs-scalable-cluster/aws"
   version = "1.1.4"
 
   # insert the 10 required variables here
@@ -41,8 +42,30 @@ module "ecs-scalable-cluster" "nexus-cluster" {
   role_policy_name = "EC2DomainJoin"
 
   # -----------------------------------------
-  # START OF NEW VARIABLES FOR ECS SERVICE/TASKS JUNK
+  # START OF NEW VARIABLES FOR ECS SERVICE/TASKS
 
   # Name of container within load balancer
   container_name = "nexus-container"
+
+  # AMI ECS image values/filter
+  ami_filter = "*-amazon-ecs-optimized"
+
+  # ECS task definition name
+  ecs_task_def_name = "nexus_task"
+
+  # ECS task definition's docker image
+#  ecs_task_def_docker_image = "mongo:latest"
+  ecs_task_def_docker_image = "647770347641.dkr.ecr.us-east-2.amazonaws.com/jeff-repo"
+
+  # ECS task definition's cpu units (max 1024)
+  ecs_task_def_cpu_units = 128
+
+  # ECS task definition's memory units
+  ecs_task_def_memory_units = 128
+
+  # ECS task definition's memory reservation
+  ecs_task_def_memory_reservation = 64
+
+  # ECS service name
+  ecs_service_name = "nexus_service"
 }
