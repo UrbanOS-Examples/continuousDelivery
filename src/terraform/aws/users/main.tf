89912a663d58da5978062a9f8a866ac7d773314c
Jeffrety Lutz <jeff@lutz.ws>
2018-05-17 10:13:18 -0400

    Switched from user creation via module to series of resources

--- src/terraform/aws/users/main.tf
+++ src/terraform/aws/users/main.tf
@@ -2,19 +2,30 @@ provider "aws" {
   region = "${var.region}"
 }
 
-module "iam_user" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-user"
+# module "iam_user" {
+#   source = "terraform-aws-modules/iam/aws//modules/iam-user"
 
-#  count         = ""
-  name          = "test.user"
+#   count                   = "${length(var.users)}"
+#   name                    = "${var.users[count.index]}"
+#   force_destroy           = true
+#   pgp_key                 = "keybase:test"
+#   password_reset_required = false
+# }
+
+resource "aws_iam_user" "user" {
+  name          = "test-user"
   force_destroy = true
-  pgp_key       = "keybase:test"
-  password_reset_required = false
+}
+
+resource "aws_iam_user_login_profile" "user-login-profile" {
+  user                    = "${aws_iam_user.user.name}"
+  pgp_key                 = "keybase:test"
+  password_reset_required = true
 }
 
 resource "aws_iam_group_membership" "superuser_membership" {
-  name = "${var.group}-membership"
-  users = "${var.users}"
-  group = "${var.group}"
-  depends_on = ["module.iam_user"]
+  name       = "${var.group}-membership"
+  users      = "${var.users}"
+  group      = "${var.group}"
+  depends_on = ["aws_iam_user.user"]
 }
