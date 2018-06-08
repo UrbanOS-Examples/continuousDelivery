8eeda14b63d20fb28ba0c7a49c9a95cf5e69411b
Chris McClellan <cmcclellan@pillartechnology.com>
2018-06-08 14:27:11 -0400

    SMRT-89 DSL-ify deployment jobs
    co-authored-by: Erin Moore <emoore@pillartechnology.com>

---
+++ src/docker/jenkins/jobs/deployments.groovy
@@ -0,0 +1,40 @@
+def repositories = [
+    'cota-streaming-producer', 
+    'cota-streaming-consumer'
+]
+
+repositories.each { repo ->
+    pipelineJob("deploy-${repo}") {
+        triggers {
+        	scm('H/2 * * * *')
+    	}
+        properties {
+            parameters {
+                parameterDefinitions {
+                    credentials {
+                        name('kubernetesCreds')
+                        description('Environment to deploy to')
+                        defaultValue('kubeconfig-dev')
+                        credentialType('com.microsoft.jenkins.kubernetes.credentials.KubeconfigCredentials')
+                        required(true)
+                    }
+                }
+            }
+        }
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url("git@github.com:SmartColumbusOS/${repo}.git")
+                            credentials('GitHub')
+                        }
+                        branch('*/master')
+                    }
+                }
+                scriptPath('Jenkinsfile.deploy')
+                lightweight()
+            }
+        }
+    }
+}
