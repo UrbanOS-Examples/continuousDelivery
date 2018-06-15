48735a764e55bdc0f502a13ea12be7fe6ac3b472
Christopher J. McClellan <cmcclellan@pillartechnology.com>
2018-06-15 15:56:27 -0400

    SMRT-91 Made proxy work correctly with websockets

--- src/docker/cota-proxy/run.sh
+++ src/docker/cota-proxy/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-envsubst < /etc/nginx/default.conf.template > /etc/nginx/conf.d/default.conf \
+envsubst '${WEBSOCKET_HOST},${WEBSOCKET_PORT},${UI_HOST},${UI_PORT}' < /etc/nginx/default.conf.template > /etc/nginx/conf.d/default.conf \
     && exec nginx -g 'daemon off;'
