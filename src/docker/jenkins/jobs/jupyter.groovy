9311914ff9b6a7e46452377e01a4b03c51ad4ee6
Erin Moore <emoore@pillartechnology.com>
2018-07-27 14:49:55 -0400

    SMRT-237 - set to push only on master
    co-authored-by: Benjamin Brewer <bbrewer@pillartechnology.com>

---
+++ src/docker/jenkins/jobs/jupyter.groovy
@@ -0,0 +1,27 @@
+def jenkinsfiles = [
+    'jupyter-notebook-scipy'
+]
+
+def repository = { pipeline_file ->
+    return {
+        scm {
+            git {
+                remote {
+                    url("git@github.com:SmartColumbusOS/jupyterhub.git")
+                    credentials('GitHub')
+                }
+                branch('*/master')
+            }
+        }
+        scriptPath("${pipeline_file}")
+        lightweight()
+    }
+}
+
+jenkinsfiles.each { file ->
+    pipelineJob(file) {
+        definition {
+            cpsScm repository("notebooks/${file}.Jenkinsfile")
+        }
+    }
+}
